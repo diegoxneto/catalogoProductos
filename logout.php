8a43960092fa9957b5b95e0e9c1bbecaaee800de
Neto <sandrapantoja@MacBook-Air-de-sandra.local>
2025-05-22 02:58:11 -0600

    Migrar api.php a PDO para PostgreSQL

--- logout.php
+++ logout.php
@@ -2,6 +2,6 @@
 session_start();
 session_unset(); // Elimina todas las variables de sesión
 session_destroy(); // Destruye la sesión
-header('Location: login.php'); // Redirige al login
+header('Location: index.php'); // Redirige al login
 exit();
 ?>
